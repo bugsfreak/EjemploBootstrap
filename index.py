33196323a87c007b8095b1c1826682ee30b11745
Raymond <raymonddamian26@hotmail.com>
2022-05-27 17:08:21 -0500

    Navbar y Carousel añadidos

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 #Se importa la libreria de flask y render_template 
 from flask import Flask, render_template
 
+#Se asigna la libreria flask a la variable app
 app = Flask(__name__, template_folder='templates')
 
+#Se define una ruta
 @app.route('/')
 def principal():
     return render_template('/prime.html')
